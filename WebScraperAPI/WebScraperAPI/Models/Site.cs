adbb2c588ddef26906664ade201a8b38f307c81f
madel <madel.asistio@gmail.com>
2021-04-23 23:45:13 -0700

    Created new ScraperController

---
+++ WebScraperAPI/WebScraperAPI/Models/Site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScraperAPI.Models
+{
+    public class Site
+    {
+        public string url;
+        public string content;
+    }
+}
